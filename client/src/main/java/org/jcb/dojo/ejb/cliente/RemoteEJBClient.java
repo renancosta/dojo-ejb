fea39ac43806d18dfc61730be68532dc72b0d902
jader <jaderbelarmino@outlook.com>
2017-05-14 12:15:38 -0300

    ejb stateful

--- client/src/main/java/org/jcb/dojo/ejb/cliente/RemoteEJBClient.java
+++ client/src/main/java/org/jcb/dojo/ejb/cliente/RemoteEJBClient.java
@@ -8,27 +8,35 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.jcb.dojo.ejb.server.HelloWorld;
-
+import org.jcb.dojo.ejb.server.StatefulHelloWorld;
 
 public class RemoteEJBClient {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("###########################\nexecutando remoto");
-        
-        invokeHelloWorld();
-    }
+	public static void main(String[] args) throws Exception {
+		System.out.println("###########################\nexecutando remoto");
 
-    private static void invokeHelloWorld() throws NamingException {
-        final HelloWorld hw = lookupRemoteHelloWorld();
-        System.out.println("############## Executando HELLO !!!");
-        System.out.println(hw.hello("jader"));
-        System.out.println(hw.historico());  
-    }
-      
+		invokeHelloWorld();
+		//invokeStatefulHelloWorld();
+	}
+
+	private static void invokeHelloWorld() throws NamingException {
+		final HelloWorld hw = lookupRemoteHelloWorld();
+		System.out.println("############## Executando HELLO !!!");
+		System.out.println(hw.hello("jader"));
+		System.out.println(hw.historico());
+	}
+	
+	private static void invokeStatefulHelloWorld() throws NamingException {
+		final StatefulHelloWorld hw = lookupRemoteStatefulHelloWorld();
+		System.out.println("############## Executando HELLO Stateful!!!");
+		System.out.println(hw.hello("jader"));
+		System.out.println(hw.historico());
+	}
+
 	private static HelloWorld lookupRemoteHelloWorld() throws NamingException {
 		final Hashtable<String, String> jndiProperties = new Hashtable<>();
-        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-        final Context context = new InitialContext(jndiProperties);
+		jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+		final Context context = new InitialContext(jndiProperties);
 
 		// The JNDI lookup name for a stateless session bean has the syntax of:
 		// ejb:<appName>/<moduleName>/<distinctName>/<beanName>!<viewClassName>
@@ -55,8 +63,42 @@ public class RemoteEJBClient {
 		// the whole package name.
 
 		// let's do the lookup
-		return (HelloWorld) context.lookup("ejb:/wildfly-ejb-remote-server-side/HelloWorldBean!" + HelloWorld.class.getName());
+		return (HelloWorld) context
+				.lookup("ejb:/wildfly-ejb-remote-server-side/HelloWorldBean!" + HelloWorld.class.getName());
 	}
 
- 
+	private static StatefulHelloWorld lookupRemoteStatefulHelloWorld() throws NamingException {
+		final Hashtable<String, String> jndiProperties = new Hashtable<>();
+		jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+		final Context context = new InitialContext(jndiProperties);
+
+		// The JNDI lookup name for a stateless session bean has the syntax of:
+		// ejb:<appName>/<moduleName>/<distinctName>/<beanName>!<viewClassName>
+		//
+		// <appName> The application name is the name of the EAR that the EJB is
+		// deployed in
+		// (without the .ear). If the EJB JAR is not deployed in an EAR then
+		// this is
+		// blank. The app name can also be specified in the EAR's
+		// application.xml
+		//
+		// <moduleName> By the default the module name is the name of the EJB
+		// JAR file (without the
+		// .jar suffix). The module name might be overridden in the ejb-jar.xml
+		//
+		// <distinctName> : EAP allows each deployment to have an (optional)
+		// distinct name.
+		// This example does not use this so leave it blank.
+		//
+		// <beanName> : The name of the session been to be invoked.
+		//
+		// <viewClassName>: The fully qualified classname of the remote
+		// interface. Must include
+		// the whole package name.
+
+		// let's do the lookup
+		return (StatefulHelloWorld) context.lookup("ejb:/wildfly-ejb-remote-server-side/StatefulHelloWorldBean!"
+				+ StatefulHelloWorld.class.getName() + "?stateful");
+	}
+
 }
